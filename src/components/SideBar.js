6b69be21d8b326ed7f2a8772ee40fb6c2ad09518
worldsong <2060884089@qq.com>
2018-04-02 15:15:17 +0800

    Make one sidebar component.

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -26,6 +26,80 @@ export default class SideBar extends Component {
                             </span>
                             </div>
                         </form>
+                        <ul className="sidebar-menu" data-widget="tree">
+                            <li className="header">MAIN NAVIGATION</li>
+                            <li className="treeview">
+                                <a href="#">
+                                    <i className="fa fa-files-o"></i>
+                                    <span>Layout Options</span>
+                                    <span className="pull-right-container">
+                                        <span className="label label-primary pull-right">4</span>
+                                    </span>
+                                </a>
+                                <ul className="treeview-menu">
+                                    <li><a href="pages/layout/top-nav.html"><i className="fa fa-circle-o"></i> Top Navigation</a></li>
+                                    <li><a href="pages/layout/boxed.html"><i className="fa fa-circle-o"></i> Boxed</a></li>
+                                    <li><a href="pages/layout/fixed.html"><i className="fa fa-circle-o"></i> Fixed</a></li>
+                                    <li><a href="pages/layout/collapsed-sidebar.html"><i className="fa fa-circle-o"></i> Collapsed Sidebar</a></li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="/">
+                                    <i className="fa fa-th"></i><span>Widgets</span>
+                                    <span className="pull-right-container">
+                                        <small className="label pull-right bg-green">new</small>
+                                    </span>
+                                </a>
+                            </li>
+                            <li className="treeview">
+                                <a href="#">
+                                    <i className="fa fa-pie-chart"></i>
+                                    <span>Charts</span>
+                                    <span className="pull-right-container">
+                                        <i className="fa fa-angle-left pull-right"></i>
+                                    </span>
+                                </a>
+                                <ul className="treeview-menu">
+                                    <li><a href="pages/charts/chartjs.html"><i className="fa fa-circle-o"></i> ChartJS</a></li>
+                                    <li><a href="pages/charts/morris.html"><i className="fa fa-circle-o"></i> Morris</a></li>
+                                    <li><a href="pages/charts/flot.html"><i className="fa fa-circle-o"></i> Flot</a></li>
+                                    <li><a href="pages/charts/inline.html"><i className="fa fa-circle-o"></i> Inline charts</a></li>
+                                </ul>
+                            </li>
+                            <li className="treeview ">
+                                <a href="#">
+                                    <i className="fa fa-table"></i> <span>Tables</span>
+                                    <span className="pull-right-container">
+                                      <i className="fa fa-angle-left pull-right"></i>
+                                    </span>
+                                </a>
+                                <ul className="treeview-menu">
+                                    <li><a href="pages/tables/simple.html"><i className="fa fa-circle-o"></i> Simple tables</a>
+                                    </li>
+                                    <li><a href="pages/tables/data.html"><i className="fa fa-circle-o"></i> Data tables</a>
+                                    </li>
+                                </ul>
+                            </li>
+                            <li>
+                                <a href="pages/calendar.html">
+                                    <i className="fa fa-calendar"></i> <span>Calendar</span>
+                                    <span className="pull-right-container">
+                                        <small className="label pull-right bg-red">3</small>
+                                        <small className="label pull-right bg-blue">17</small>
+                                    </span>
+                                </a>
+                            </li>
+                            <li>
+                                <a href="pages/mailbox/mailbox.html">
+                                    <i className="fa fa-envelope"></i><span>Mailbox</span>
+                                    <span className="label pull-right-container">
+                                        <small className="label pull-right bg-yellow">12</small>
+                                        <small className="label pull-right bg-green">16</small>
+                                        <small className="label pull-right bg-red">5</small>
+                                    </span>
+                                </a>
+                            </li>
+                        </ul>
                     </section>
                 </aside>
                 <div className="content-wrapper">
